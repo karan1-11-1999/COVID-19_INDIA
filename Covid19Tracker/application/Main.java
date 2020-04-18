4f86643cae89bc08fb2fe44ded9d4540ab81e6e0
karan <khpatel4@wisc.edu>
2020-04-19 01:09:26 +0530

    Version 3 Dummy Graph implemented
    Dummy graph implemented.
    Can someone create an array of a 2D array of DATE and NO OF CASES ON THAT DAY?
    So I can use that and form a real graph

--- Covid19Tracker/application/Main.java
+++ Covid19Tracker/application/Main.java
@@ -3,12 +3,19 @@ package application;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -21,6 +28,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	// store any command-line arguments that were entered.
 	// NOTE: this.getParameters().getRaw() will get these also
+	
+	private static JsonParser jp;
+	private static Patient[] patients;
+	
 	private List<String> args;
 
 	private static final int WINDOW_WIDTH = 954;
@@ -82,8 +93,14 @@ public class Main extends Application {
    		rightImgView.setFitHeight(500);
 		root.setRight(rightImgView);
 		
-		// Main layout is Border Pane example (top,left,center,right,bottom)
-        
+		
+
+		//center
+		root.setCenter(drawCountryGraph());
+		
+		
+		
+		
         Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 		// Add the stuff and set the primary stage
         primaryStage.setTitle(APP_TITLE);
@@ -93,16 +110,97 @@ public class Main extends Application {
         
 	}
 
+	private LineChart drawCountryGraph() {
+		
+		/*
+		 * 
+		 * 
+		 * 
+		 * 
+		 * IMPORTANT TODO
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		//WE NEED DATA FROM PATIENT CLASS ABOUT
+		//EACH DAY'S NUMBER OF CASES 
+		
+		//WANNA FILL DATE 
+		//data.getData().add(new XYChart.Data<String,Number>("DATE STRING",nO Of cAsES ON tHaT dAY));
+
+		//So for that we need a 2D array of DATE and NO OF CASES ON THAT DAY
+		// 
+		// PLEASE SOME CREATE AND FILL IN THE ARRAY SO I CAN CREATE A GRAPH
+		
+		/*
+		 * 
+		 * 
+		 * 
+		 * 
+		 * IMPORTANT TODO
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		ObservableList<String> ob = FXCollections.observableArrayList();
+		//https://www.youtube.com/watch?v=LyeQxISSqJs
+		
+		CategoryAxis xAxis = new CategoryAxis();
+		xAxis.setLabel("Date");
+		
+		NumberAxis yAxis = new NumberAxis();
+		yAxis.setLabel("No of positive cases");
+		
+		LineChart<String, Number> lc = new LineChart<String, Number>(xAxis, yAxis);
+		lc.setTitle("India Cases");
+		
+		
+		XYChart.Series<String, Number> data = new XYChart.Series<String, Number>();
+		
+		//data.getData().add(new XYChart.Data<String,Number>("DATE STRING WITH TRUNCATE",CASES));
+		//DUMMY DATA!!!
+		data.getData().add(new XYChart.Data<String,Number>("12/4",207));
+		data.getData().add(new XYChart.Data<String,Number>("13/4",342));
+		data.getData().add(new XYChart.Data<String,Number>("14/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("15/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("16/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("17/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("18/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("19/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("20/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("21/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("22/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("23/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("24/4",750));
+		data.getData().add(new XYChart.Data<String,Number>("25/4",750));
+				
+		data.setName("Confirmed");
+		
+		lc.getData().add(data);
+		return lc;
+		
+		
+		//Test
+		//System.out.println(patients[0].getAge());
+		
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {   
 		
 		//calling classes and methods
-        JsonParser jp = new JsonParser();
+        jp = new JsonParser();
         //Creates a txt file from the live URL and then stores in Patient Object Array
         jp.liveURLToJSONToPatientObj(); 
-        Patient[] patients = jp.getPatients();
+        patients = jp.getPatients();
         
         //WHITEBOX TESTING
         //
@@ -114,7 +212,7 @@ public class Main extends Application {
         //
         //
         //WHITEBOX TESTING OVER
-        		
+        
         //launching the javafx
 		launch(args);
 		   
